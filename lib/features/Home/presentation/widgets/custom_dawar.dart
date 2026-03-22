6fc03122f3c0dc4079804493d3c513466cfd750d
Mohamed Atta <mohamedatta24@gmail.com>
2026-03-22 07:07:53 +0200

    finsh  custom dawar

--- lib/features/Home/presentation/widgets/custom_dawar.dart
+++ lib/features/Home/presentation/widgets/custom_dawar.dart
@@ -19,7 +19,7 @@ class CustomDawar extends StatelessWidget {
             hasScrollBody: false,
             child: Column(
               children: const [
-                CustomDawar(),
+                CustomUserItem(),
                 SizedBox(height: 16.0),
                 DawarItemListView(),
                 Expanded(child: SizedBox(height: 20)),
